e4f0849e80e9c3c3c9d49fb7827b0828540b52bc
Sebastien Binet <binet@cern.ch>
2016-01-28 16:47:24 +0100

    _examples/run-hist: update

--- _examples/run-hist.go
+++ _examples/run-hist.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gonum/plot"
 	"github.com/gonum/plot/plotter"
 	"github.com/gonum/plot/vg"
-	"github.com/gonum/plot/vg/vgdraw"
+	"github.com/gonum/plot/vg/draw"
 )
 
 var (
@@ -82,16 +82,16 @@ func stdNorm(x float64) float64 {
 
 type GnuplotStyle struct{}
 
-func (s GnuplotStyle) DrawPlot(p *plot.Plot, c vgdraw.Canvas) {
+func (s GnuplotStyle) DrawPlot(p *plot.Plot, c draw.Canvas) {
 	if p.BackgroundColor != nil {
 		c.SetColor(p.BackgroundColor)
-		c.Fill(c.Rect.Path())
+		c.Fill(c.Rectangle.Path())
 	}
 	if p.Title.Text != "" {
 		cx := p.DataCanvas(c)
-		c.FillText(p.Title.TextStyle, cx.Center().X, c.Max().Y, -0.5, -1, p.Title.Text)
-		c.Size.Y -= p.Title.Height(p.Title.Text) - p.Title.Font.Extents().Descent
-		c.Size.Y -= p.Title.Padding
+		c.FillText(p.Title.TextStyle, cx.Center().X, c.Max.Y, -0.5, -1, p.Title.Text)
+		c.Max.Y -= p.Title.Height(p.Title.Text) - p.Title.Font.Extents().Descent
+		c.Max.Y -= p.Title.Padding
 	}
 
 	p.X.SanitizeRange()
@@ -102,25 +102,24 @@ func (s GnuplotStyle) DrawPlot(p *plot.Plot, c vgdraw.Canvas) {
 	ywidth := y.Size()
 	xheight := x.Size()
 
-	xda := plot.PadX(p, c.Crop(ywidth-y.Width-y.Padding, 0, 0, 0))
-	yda := plot.PadY(p, c.Crop(0, xheight-x.Width-x.Padding, 0, 0))
+	xda := plot.PadX(p, draw.Crop(c, ywidth-y.Width-y.Padding, 0, 0, 0))
+	yda := plot.PadY(p, draw.Crop(c, 0, xheight-x.Width-x.Padding, 0, 0))
 
 	x.Draw(xda)
 	y.Draw(yda)
 	xmin := xda.Min.X
-	xmax := xda.Max().X
+	xmax := xda.Max.X
 	ymin := yda.Min.Y
-	ymax := xda.Max().Y
+	ymax := xda.Max.Y
 	xda.StrokeLine2(x.LineStyle, xmin, ymax, xmax, ymax)
 	xda.StrokeLine2(x.LineStyle, xmin, ymin, xmax, ymin)
 	yda.StrokeLine2(y.LineStyle, xmin, ymin, xmin, ymax)
 	yda.StrokeLine2(y.LineStyle, xmax, ymin, xmax, ymax)
 
-	datac := plot.PadY(p, plot.PadX(p, c.Crop(ywidth, xheight, 0, 0)))
+	datac := plot.PadY(p, plot.PadX(p, draw.Crop(c, ywidth, xheight, 0, 0)))
 	for _, data := range p.Plotters() {
 		data.Plot(datac, p)
 	}
 
-	p.Legend.Draw(c.Crop(ywidth, 0, 0, 0).Crop(0, xheight, 0, 0))
-
+	p.Legend.Draw(draw.Crop(draw.Crop(c, ywidth, 0, 0, 0), 0, 0, xheight, 0))
 }
